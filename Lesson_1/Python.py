f23457d7232f8ea311294b467d64762f32f1455d
TJMihal <puzirev.misha!gmail.com>
2026-02-28 11:30:11 +0300

    Комит

---
+++ Lesson_1/Python.py
@@ -0,0 +1,25 @@
+## Идентификаторы
+
+print(42)
+print(42.5)
+print("Text")
+print(True)
+print(False)
+
+## Переменная
+
+firstName = "Михаил"
+print(firstName)
+age = 31
+print(age)
+city = "Moscow"
+print(city)
+
+
+
+a = 10
+print(2 + a)
+print(42 * a)
+print(42 - a)
+
+b = None
