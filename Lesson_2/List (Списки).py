c3abee1cb93c2e85b5657216f89936122636d192
TJMihal <puzirev.misha!gmail.com>
2026-03-01 21:57:24 +0300

    Добавлены домашние задания по словарям и типам данных

---
+++ Lesson_2/List (Списки).py
@@ -0,0 +1,27 @@
+films_list = ["Мстители", "Железный человек", 2026, True, 'Человек паук']
+
+print(films_list)
+print(films_list[2])
+print(films_list[3])
+print(films_list[0])
+print(films_list[-1])
+print(films_list[1])
+
+films_list[2] = 2025
+print(films_list)
+
+a = '-'
+print(a * 40)
+
+my_list = []
+my_list = list()
+my_list.append(2025)
+my_list.append('text')
+print(my_list)
+print(len(my_list))
+print(my_list.index('text'))
+my_list.pop(0)
+print(my_list)
+
+
+
---
+++ Lesson_2/Преобразование типов данных.py
@@ -0,0 +1,4 @@
+user_input = input('Enter a number')
+print(type(user_input))
+
+print(2 + user_input)
